78f5d8fd638bbd861342f7e5b2d1998875887dbb
Cory Pease <coryrpease@gmail.com>
2019-09-08 13:38:47 -0600

    can join extrenally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc';
 export class AppComponent {
   title = 'video-confernce-app';
   localStream: Stream
+  remoteCalls:any = [];
 
   constructor(
     private agoraService: AngularAgoraRtcService
@@ -44,6 +45,46 @@ export class AppComponent {
       });
     }, function (err) {
       console.log("getUserMedia failed", err);
+    });
+
+    this.agoraService.client.on('error', (err) => {
+      console.log("Got error msg:", err.reason);
+      if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+        this.agoraService.client.renewChannelKey("", () => {
+          console.log("Renew channel key successfully");
+        }, (err) => {
+          console.log("Renew channel key failed: ", err);
+        });
+      }
+    });
+
+    this.agoraService.client.on('stream-added', (evt) => {
+      const stream = evt.stream;
+      this.agoraService.client.subscribe(stream, (err) => {
+        console.log("Subscribe stream failed", err);
+      });
+    });
+
+    this.agoraService.client.on('stream-subscribed', (evt) => {
+      const stream = evt.stream;
+      if (!this.remoteCalls.includes(`agora_remote${stream.getId()}`)) this.remoteCalls.push(`agora_remote${stream.getId()}`);
+      setTimeout(() => stream.play(`agora_remote${stream.getId()}`), 2000);
+    });
+
+    this.agoraService.client.on('stream-removed', (evt) => {
+      const stream = evt.stream;
+      stream.stop();
+      this.remoteCalls = this.remoteCalls.filter(call => call !== `#agora_remote${stream.getId()}`);
+      console.log(`Remote stream is removed ${stream.getId()}`);
+    });
+
+    this.agoraService.client.on('peer-leave', (evt) => {
+      const stream = evt.stream;
+      if (stream) {
+        stream.stop();
+        this.remoteCalls = this.remoteCalls.filter(call => call === `#agora_remote${stream.getId()}`);
+        console.log(`${evt.uid} left from this channel`);
+      }
     });
   }
 }
