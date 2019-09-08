20aa5df2e6a01ee237ca704dc551d5d045d347b2
Cory Pease <coryrpease@gmail.com>
2019-09-08 13:22:26 -0600

    importing agora into modules'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc';
 
 import { AppComponent } from './app.component';
+
+const agoraConfig: AgoraConfig = {
+  AppID: '8690cdef67694df8ac477960cf8420ed',
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
