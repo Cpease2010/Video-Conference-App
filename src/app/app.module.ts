62a598503bba137407ec062f70e0588a0c5c5541
Cory Pease <coryrpease@gmail.com>
2019-09-08 15:10:26 -0600

    basic style, able to connect from external sources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc';
+import {MatCardModule} from '@angular/material/card'
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 const agoraConfig: AgoraConfig = {
   AppID: '8690cdef67694df8ac477960cf8420ed',
@@ -14,7 +16,9 @@ const agoraConfig: AgoraConfig = {
   ],
   imports: [
     BrowserModule,
-    AngularAgoraRtcModule.forRoot(agoraConfig)
+    AngularAgoraRtcModule.forRoot(agoraConfig),
+    NoopAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
